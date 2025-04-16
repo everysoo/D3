09b589bc54b7dd466e303c018c489a46b492f817
eunsoo_joe <joeun2264@gmail.com>
2025-04-16 17:46:43 +0900

    add winnerColor

--- js/index.js
+++ js/index.js
@@ -2,6 +2,26 @@ var maxRadius = 35;
 var rScale = d3.scaleSqrt().domain([0, 210]).range([0, maxRadius]);
 var popup;
 
+var colorScale={
+  "France": '#016FB9',
+  "Luxembourg": '#C6E0FF',
+  "Belgium": '#555',
+  "Italy": '#58BC82',
+  "Switzerland": '#D16462',
+  "Spain": '#FFC400',
+  "Netherlands": '#F86018',
+  "USA": '#74B0D8',
+  "Ireland Irish": '#FFF',
+  "Denmark": '#EBBCBB',
+  "Germany": '#FFCE00',
+  "Australia": '#013D65',
+  "United Kingdom": '#B80C09',
+  "Results voided": 'none'
+}
+
+
+
+// 차트 그리드
 function layout(data) {
   var cellSize = 80, numCols = 10;
   data.forEach(function(d) {
@@ -16,9 +36,11 @@ function layout(data) {
 
     d.layout.entrantsRadius = rScale(d.Entrants);
     d.layout.finishersRadius = rScale(d.Finishers);
+    d.layout.winnerColor = colorScale[d["Winner's Nationality"].trim()];
   });
 }
 
+// 마우스 오버 팝업 기능
 function popupTemplate(d) {
   var year = +d.Year;
   var distance = +d["Total distance (km)"];
@@ -39,26 +61,29 @@ function popupTemplate(d) {
   return html;
 }
 
+// 차트 렌더링
 function updateChart(data) {
-  layout(data);
+  layout(data);                             // layout 함수 불러옴
 
-  d3.select('svg g.chart g.entrants')
-    .selectAll('circle')
-    .data(data)
-    .join('circle')
+  // entrants
+  d3.select('svg g.chart g.entrants')       // html 태그 연결
+    .selectAll('circle')                    // 가상의 circle 생성
+    .data(data)                             // data 연결
+    .join('circle')                         // data-circle 연결
     .attr('cx', function(d) {
-      return d.layout.x;
+      return d.layout.x;                    // 원의 x 배치
     })
     .attr('cy', function(d) {
-      return d.layout.y;
+      return d.layout.y;                    // 원의 y 배치
     })
     .attr('r', function(d) {
-      return d.layout.entrantsRadius;
+      return d.layout.entrantsRadius;       // 반지름 설정
     })
     .style('fill', 'none')
     .style('stroke', '#aaa')
     .style('stroke-dasharray', '1 1');
 
+  // finishers
   d3.select('svg g.chart g.finishers')
     .selectAll('circle')
     .data(data)
@@ -74,15 +99,32 @@ function updateChart(data) {
     })
     .style('fill', '#aaa')
     .on('mousemove', function(d) {
-      popup.point(d3.event.clientX, d3.event.clientY);
-      popup.html(popupTemplate(d));
-      popup.draw();
+      popup.point(d3.event.clientX, d3.event.clientY);      // 커서 감시
+      popup.html(popupTemplate(d));                         // html화
+      popup.draw();                                         // 렌더링
     })
     .on('mouseout', function() {
       popup.hide();
     });
+
+  d3.select('svg g.chart g.winners')
+    .selectAll('circle')
+    .data(data)
+    .join('circle')
+    .attr('cx', function(d){
+      return d.layout.x;
+    })
+    .attr('cy', function(d){
+      return d.layout.y;
+    })
+    .attr('r', 5)
+    .style('pointer-events', 'none')
+    .style('fill', function(d){
+      return d.layout.winnerColor;
+    })
 }
 
+// 콜백
 function dataIsReady(data) {
   updateChart(data);
   popup = Popup();
